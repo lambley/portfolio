d5a33a832feda8a0c9660f0aa9e3065e41d3507f
Aaron Lambley <aaron.lambley@gmail.com>
2023-08-16 15:30:47 +0100

    add icons for theme toggle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+
 function MyApp({ Component, pageProps }: AppProps) {
   const [theme, setTheme] = useState("light");
 
@@ -23,8 +26,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className={`${theme}-theme`} style={{ minHeight: "100vh" }}>
       <Layout>
-        <button onClick={toggleTheme}>
-          {theme === "light" ? "Dark" : "Light"} Mode
+        <button className="theme-toggle" onClick={toggleTheme}>
+          {theme === "light" ? (
+            <FontAwesomeIcon icon={faMoon} />
+          ) : (
+            <FontAwesomeIcon icon={faSun} />
+          )}
         </button>
         <Component {...pageProps} />
       </Layout>
