155cf62925c1aadd7954c0c8c81180a66b296d9c
Aaron Lambley <aaron.lambley@gmail.com>
2023-08-16 12:36:09 +0100

    regorganise file structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomePage = (): JSX.Element => {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptate, voluptatum, voluptas, dolorum quod quos voluptatem
+        reprehenderit consequatur quia quae numquam? Quisquam, quia. Quasi
+        voluptatibus, quos voluptatem nesciunt quae quia.
+      </p>
+    </>
+  );
+};
+
+export default HomePage;
