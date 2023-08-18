7f2450938b5bd8d075413a0190c9aade3e514aa0
Aaron Lambley <aaron.lambley@gmail.com>
2023-08-18 11:49:56 +0100

    add back button styling

--- src/pages/portfolio/[portfolioId].tsx
+++ src/pages/portfolio/[portfolioId].tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretLeft } from "@fortawesome/free-solid-svg-icons";
 
 const PortfolioItem: React.FC = () => {
   const router = useRouter();
   const { portfolioId } = router.query;
 
+  const handleBack = () => {
+    router.push("/portfolio");
+  };
+
   return (
     <div className="container text-center">
-      <button>Back</button>
+      <button
+        className="portfolio-back-button"
+        aria-label="back button"
+        onClick={handleBack}
+      >
+        <FontAwesomeIcon icon={faCaretLeft} /> Back
+      </button>
       <h1>Portfolio Item</h1>
       <p>Portfolio ID: {portfolioId}</p>
     </div>
