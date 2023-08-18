7e26c33fcd264d0a5c88dad4745f871af958e256
Aaron Lambley <aaron.lambley@gmail.com>
2023-08-18 09:36:50 +0100

    add navigation tests

---
+++ src/components/__tests__/Navigation.test.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Navigation from "../Navigation";
+
+describe("Navigation", () => {
+  it("renders the navigation links", () => {
+    render(
+      <Navigation>
+        <div>Test Child Content</div>
+      </Navigation>
+    );
+    const homeLink = screen.getByText("Home");
+    const blogLink = screen.getByText("Blog");
+    const portfolioLink = screen.getByText("Portfolio");
+
+    expect(homeLink).toBeInTheDocument();
+    expect(blogLink).toBeInTheDocument();
+    expect(portfolioLink).toBeInTheDocument();
+  });
+
+  it("renders the child content", () => {
+    render(
+      <Navigation>
+        <div>Test Child Content</div>
+      </Navigation>
+    );
+    const childContent = screen.getByText("Test Child Content");
+
+    expect(childContent).toBeInTheDocument();
+  });
+
+  // Update links when we have the pages
+  it("renders the navigation links with the correct href", () => {
+    render(
+      <Navigation>
+        <div>Test Child Content</div>
+      </Navigation>
+    );
+    const homeLink = screen.getByText("Home");
+    const blogLink = screen.getByText("Blog");
+    const portfolioLink = screen.getByText("Portfolio");
+
+    expect(homeLink.getAttribute("href")).toBe("/");
+    expect(blogLink.getAttribute("href")).toBe("/");
+    expect(portfolioLink.getAttribute("href")).toBe("/");
+  });
+});
