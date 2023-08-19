47a309e4f8be3e20d1fce29727cc28e9d41b010c
Aaron Lambley <aaron.lambley@gmail.com>
2023-08-19 12:12:10 +0100

    move PortfolioType to Type file

--- custom.d.ts
+++ custom.d.ts
@@ -3,3 +3,16 @@ import { PrismaClient } from '@prisma/client';
 declare global {
   var prisma: PrismaClient;
 }
+
+type PortfolioType = {
+  id: number;
+  title: string;
+  description: string;
+  url?: string;
+  repoUrl?: string;
+  image: string;
+  category: string[];
+  date: string;
+}
+
+export type { PortfolioType };
