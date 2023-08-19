677982238bc321d81ef23f3680879bb8e1623118
Aaron Lambley <aaron.lambley@gmail.com>
2023-08-19 12:02:03 +0100

    add getStaticProps for portfolio items

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,49 +1,35 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import prisma from "../../../lib/prisma";
+import { GetStaticProps } from "next";
 
-const Portfolio: React.FC = () => {
-  const portfolioSampleData = [
-    {
-      portfolioId: "1",
-      portfolioName: "Portfolio 1",
-      portfolioDescription: "Portfolio 1 description",
-    },
-    {
-      portfolioId: "2",
-      portfolioName: "Portfolio 2",
-      portfolioDescription: "Portfolio 2 description",
-    },
-    {
-      portfolioId: "3",
-      portfolioName: "Portfolio 3",
-      portfolioDescription: "Portfolio 3 description",
-    },
-    {
-      portfolioId: "4",
-      portfolioName: "Portfolio 4",
-      portfolioDescription: "Portfolio 4 description",
-    },
-    {
-      portfolioId: "5",
-      portfolioName: "Portfolio 5",
-      portfolioDescription: "Portfolio 5 description",
-    },
-    {
-      portfolioId: "6",
-      portfolioName: "Portfolio 6",
-      portfolioDescription: "Portfolio 6 description",
-    },
-  ];
+interface PortfolioProps {
+  feed: any;
+}
+
+type PortfolioType = {
+  id: number;
+  title: string;
+  description: string;
+  url?: string;
+  repoUrl?: string;
+  image: string;
+  category: string[];
+  date: string;
+}
+
+const Portfolio: React.FC<PortfolioProps> = (props) => {
+  const { feed } = props;
 
   const renderPortfolioList = () => {
-    return portfolioSampleData.map((portfolio) => (
-      <div className="portfolio-item" key={portfolio.portfolioId}>
+    return feed.map((portfolio:PortfolioType) => (
+      <div className="portfolio-item" key={portfolio.id}>
         <Link
           className="portfolio-link"
-          href={`/portfolio/${portfolio.portfolioId}`}
+          href={`/portfolio/${portfolio.id}`}
         >
-          <div className="portfolio-title">{portfolio.portfolioName}</div>
+          <div className="portfolio-title">{portfolio.title}</div>
           <Image
             src="https://placehold.co/300x200/png"
             alt=""
@@ -52,7 +38,7 @@ const Portfolio: React.FC = () => {
             height={200}
           />
           <div className="portfolio-description">
-            {portfolio.portfolioDescription}
+            {portfolio.description}
           </div>
         </Link>
       </div>
@@ -68,3 +54,11 @@ const Portfolio: React.FC = () => {
 };
 
 export default Portfolio;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const feed = await prisma.portfolio.findMany();
+  return {
+    props: { feed },
+    revalidate: 10,
+  };
+};
