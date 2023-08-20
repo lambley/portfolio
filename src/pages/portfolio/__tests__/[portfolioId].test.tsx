72475ca117dda1cb2e001e9d477e231eccd8fe57
Aaron Lambley <aaron.lambley@gmail.com>
2023-08-20 15:01:40 +0100

    change to getStaticProps

--- src/pages/portfolio/__tests__/[portfolioId].test.tsx
+++ src/pages/portfolio/__tests__/[portfolioId].test.tsx
@@ -7,36 +7,46 @@ jest.mock("next/router", () => ({
   useRouter: () => mockPortfolioUseRouter(),
 }));
 
+const mockPortfolio = {
+  id: 1,
+  title: "Test Portfolio",
+  description: "Test description",
+  url: "https://www.example.com",
+  repoUrl: "https://www.github.com/example",
+  image: "test.jpg",
+  categories: ["Category 1", "Category 2"],
+  date: "2023-08-19",
+};
+
 describe("PortfolioItem", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   it("renders the portfolio item page", () => {
-    const router = mockPortfolioUseRouter({});
+    render(<PortfolioItem portfolio={mockPortfolio} />);
 
-    render(<PortfolioItem />);
+    const title = screen.getByText("Test Portfolio");
+    const description = screen.getByText("Test description");
+    const urlLink = screen.getByText("https://www.example.com");
+    const repoLink = screen.getByText("https://www.github.com/example");
+    const category1 = screen.getByText("Category 1");
+    const category2 = screen.getByText("Category 2");
+    const backButton = screen.getByLabelText("back button");
 
-    const portfolioHeader = screen.getByText("Portfolio Item");
-    const portfolioDescription = screen.getByText("Portfolio ID: 1");
-
-    expect(portfolioHeader).toBeInTheDocument();
-    expect(portfolioDescription).toBeInTheDocument();
-  });
-
-  it("renders the portfolio item page with the correct portfolio id", () => {
-    const router = mockPortfolioUseRouter({});
-
-    render(<PortfolioItem />);
-
-    const portfolioDescription = screen.getByText("Portfolio ID: 1");
-    expect(portfolioDescription).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+    expect(urlLink).toHaveAttribute("href", "https://www.example.com");
+    expect(repoLink).toHaveAttribute("href", "https://www.github.com/example");
+    expect(category1).toBeInTheDocument();
+    expect(category2).toBeInTheDocument();
+    expect(backButton).toBeInTheDocument();
   });
 
   it("renders the portfolio item with a working back button", () => {
     const router = mockPortfolioUseRouter({});
 
-    render(<PortfolioItem />);
+    render(<PortfolioItem portfolio={mockPortfolio} />);
 
     const backButton = screen.getByLabelText("back button");
     expect(backButton).toBeInTheDocument();
