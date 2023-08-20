f2e7c1fe0fdf7d219cc6b24d4a3ecada08a7c838
Aaron Lambley <aaron.lambley@gmail.com>
2023-08-20 15:29:29 +0100

    update portfolio index test

--- src/pages/portfolio/__tests__/index.test.tsx
+++ src/pages/portfolio/__tests__/index.test.tsx
@@ -3,8 +3,31 @@ import { render, screen } from "@testing-library/react";
 import Portfolio from "../index";
 
 describe("Portfolio", () => {
+  const mockPortfolio = [
+    {
+      id: 1,
+      title: "test title",
+      description: "test description",
+      image: "test image",
+      url: "test url",
+      repoUrl: "test repo url",
+      categories: ["test category"],
+      date: "test date",
+    },
+    {
+      id: 2,
+      title: "test title 2",
+      description: "test description 2",
+      image: "test image 2",
+      url: "test url 2",
+      repoUrl: "test repo url 2",
+      categories: ["test category 2"],
+      date: "test date 2",
+    }
+  ];
+
   it("renders the portfolio page", () => {
-    render(<Portfolio />);
+    render(<Portfolio feed={mockPortfolio} />);
     const portfolioHeader = screen.getByText("Portfolio List");
 
     expect(portfolioHeader).toBeInTheDocument();
