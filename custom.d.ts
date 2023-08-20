43fd59579bf87018dca298e069ca051d28bfebdd
Aaron Lambley <aaron.lambley@gmail.com>
2023-08-20 15:01:31 +0100

    allow undefined and null urls

--- custom.d.ts
+++ custom.d.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 declare global {
   var prisma: PrismaClient;
@@ -8,11 +8,11 @@ type PortfolioType = {
   id: number;
   title: string;
   description: string;
-  url?: string;
-  repoUrl?: string;
+  url?: string | null | undefined;
+  repoUrl?: string | null | undefined;
   image: string;
   categories: string[];
   date: string;
-}
+};
 
 export type { PortfolioType };
