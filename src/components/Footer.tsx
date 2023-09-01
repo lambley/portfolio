406419d69b6a6db361d6bc2d0b0c365b7a886cb5
Aaron Lambley <aaron.lambley@gmail.com>
2023-09-01 09:20:42 +0100

    add socials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,36 @@
 import React from "react";
 import VisitorCounter from "./VisitorCounter";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = (): JSX.Element => {
   return (
     <footer className="footer text-center">
       <div className="footer-content">
         <VisitorCounter />
+        <div className="footer-socials">
+          <a
+            href="https://github.com/lambley/"
+            target="_blank"
+            aria-label="Go to my GitHub"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/aaron-lambley-35671b124/"
+            target="_blank"
+            aria-label="Go to my LinkedIn"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a
+            href="https://www.instagram.com/maya_the_collie_cross/"
+            target="_blank"
+            aria-label="Go to my Instagram"
+          >
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+        </div>
         <span className="footer-text">
           &copy; {new Date().getFullYear()} Aaron Lambley
         </span>
