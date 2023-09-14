9311d001ff610d6f4f972ec08edceaa5c5c68c2b
Aaron Lambley <aaron.lambley@gmail.com>
2023-09-14 11:57:06 +0100

    filter draft posts from blog feed

--- src/pages/blog/__tests__/index.test.tsx
+++ src/pages/blog/__tests__/index.test.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 import { render, screen } from "@testing-library/react";
-import Blog from "../index";
+import Blog, { getStaticProps } from "../index";
 import { mockBlog, mockBlogList } from "@/utils/constants/mockPortfolio";
+import apiUrl from "@/utils/apiConfig";
+import { BlogType } from "../../../../custom";
 
 describe("Blog", () => {
   const mockFeed = mockBlogList;
@@ -13,10 +17,33 @@ describe("Blog", () => {
     expect(blogHeader).toBeInTheDocument();
   });
 
-  it("should render the blog list", () => {
-    render(<Blog feed={mockFeed} />);
+  it("should render the blog list", async () => {
+    +render(<Blog feed={mockFeed} />);
     const blogItems = screen.getAllByLabelText("blog-item");
 
     expect(blogItems).toHaveLength(2);
+  });
+
+  describe("getStaticProps", () => {
+    it("should return the blog feed", async () => {
+      const mockFeedWithDraft = [
+        ...mockFeed,
+        {
+          ...mockBlog,
+          status: "draft",
+        },
+      ];
+
+      const mock = new MockAdapter(axios);
+
+      mock.onGet(`${apiUrl}/api/v1/blogs`).reply(200, mockFeedWithDraft);
+
+      const response = (await getStaticProps({})) as {
+        props: { feed: BlogType[] };
+      };
+      expect(response.props.feed).toHaveLength(mockFeed.length);
+
+      mock.reset();
+    });
   });
 });
