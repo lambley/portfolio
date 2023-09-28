3b4401cd8230ec58160b63930d1fd753aa2f073a
Aaron Lambley <aaron.lambley@gmail.com>
2023-09-28 12:24:20 +0100

    create card styling for latest posts

--- src/components/Latest.tsx
+++ src/components/Latest.tsx
@@ -4,11 +4,14 @@ import {
   notFoundPortfolio,
   notFoundBlog,
 } from "@/utils/constants/notFoundTypes";
+import Link from "next/link";
+import Image from "next/image";
 import axios from "axios";
 import apiUrl from "@/utils/apiConfig";
 import Loading from "./Loader/Loading";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Row, Col } from "react-bootstrap";
+import { toTitleCase } from "@/utils/stringUtils";
+import moment from "moment";
 
 const Latest = () => {
   const [portfolio, setPortfolio] = useState<PortfolioType>(notFoundPortfolio);
@@ -45,21 +48,66 @@ const Latest = () => {
     return <div className="my-3">{error}</div>;
   }
 
+  const renderPortfolio = () => {
+    return (
+      <Col xs={12} lg={6} className="text-center">
+        <h2>Latest Project</h2>
+        <Link
+          href={`/portfolio/${portfolio.id}`}
+          aria-label="portfolio-item-link"
+          className="latest-link border rounded"
+        >
+          <Image
+            src={
+              `/images/${portfolio.image}.png` ||
+              "https://placehold.co/300x200/png"
+            }
+            alt={portfolio.title}
+            width={100}
+            height={100}
+            className="latest-link-image border-right"
+          />
+          <div className="latest-link-text">
+            <p>{toTitleCase(portfolio.title)}</p>
+            <small>{moment(portfolio.created_at).format("DD MMM 'YY")}</small>
+          </div>
+        </Link>
+      </Col>
+    );
+  };
+
+  const renderBlog = () => {
+    return (
+      <Col xs={12} lg={6} className="text-center">
+        <h2>Latest Blog</h2>
+        <Link
+          href={`/blog/${blog.id}`}
+          aria-label="blog-item-link"
+          className="latest-link border rounded"
+        >
+          <Image
+            src={
+              `/images/${blog.image}.png` || "https://placehold.co/300x200/png"
+            }
+            alt={blog.title}
+            width={100}
+            height={100}
+            className="latest-link-image border-right"
+          />
+          <div className="latest-link-text">
+            <p>{toTitleCase(blog.title)}</p>
+            <small>{moment(blog.created_at).format("DD MMM 'YY")}</small>
+          </div>
+        </Link>
+      </Col>
+    );
+  };
+
   return (
-    <div className="my-3">
-      <Row>
-        <Col xs={12} lg={6}>
-          <div>
-            <h2>Latest Project</h2>
-            <p>{portfolio.title}</p>
-          </div>
-        </Col>
-        <Col xs={12} lg={6}>
-          <div>
-            <h2>Latest Blog</h2>
-            <p>{blog.title}</p>
-          </div>
-        </Col>
+    <div className="my-5">
+      <Row className="g-5">
+        {renderBlog()}
+        {renderPortfolio()}
       </Row>
     </div>
   );
