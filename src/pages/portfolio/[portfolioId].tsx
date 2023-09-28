ff73d201be9d28e671095718eb94d749104467e0
Aaron Lambley <aaron.lambley@gmail.com>
2023-09-28 12:46:15 +0100

    fix: portfolio backbutton

--- src/pages/portfolio/[portfolioId].tsx
+++ src/pages/portfolio/[portfolioId].tsx
@@ -22,7 +22,7 @@ const PortfolioItem: React.FC<PortfolioItemProps> = ({ portfolio }) => {
   const router = useRouter();
 
   const handleBack = () => {
-    router.back();
+    window.history.back();
   };
 
   const renderCategories = (categories: string[]) => {
