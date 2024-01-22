db4094cfb9291a95c4bc9a5d6d1c0aa9616cf3c7
Aaron Lambley <aaron.lambley@gmail.com>
2024-01-22 15:52:27 +0000

    Update [blogId].test.tsx and mockUseRouter.ts

--- src/utils/tests/mocks/mockUseRouter.ts
+++ src/utils/tests/mocks/mockUseRouter.ts
@@ -1,6 +1,7 @@
 export const mockPortfolioUseRouter = (props: Record<string, any> = {}) => ({
-  query: { portfolioId: '1' },
+  query: { portfolioId: "1" },
   push: jest.fn(),
+  back: jest.fn(),
   pathname: "",
   asPath: "",
   ...props,
