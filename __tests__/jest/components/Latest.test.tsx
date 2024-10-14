b097391ff2ca6d76e5f1e2a7a9df6ea4418040b0
Aaron Lambley <aaron.lambley@gmail.com>
2024-10-14 12:14:59 +0100

    fix Latest component test

--- __tests__/jest/components/Latest.test.tsx
+++ __tests__/jest/components/Latest.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Latest from "../../../src/components/Latest";
-import { render, waitFor, screen } from "@testing-library/react";
+import Latest from "@/components/Latest";
+import { render, waitFor, screen, act } from "@testing-library/react";
 import { mockPortfolio, mockBlog } from "@/utils/constants/mockPortfolio";
 import axios from "axios";
 
@@ -12,19 +12,19 @@ const mockResponseData = {
 };
 
 describe("Latest", () => {
-  beforeAll(() => {
-    // reset everything before each test
+  beforeEach(() => {
     jest.clearAllMocks();
-    jest.spyOn(React, "useEffect").mockReset();
-
-    // mock axios
-    (axios.get as jest.Mock).mockResolvedValueOnce({
-      data: mockResponseData,
-    });
+    jest.useFakeTimers();
   });
 
   it("renders correctly", async () => {
-    render(<Latest />);
+    (axios.get as jest.Mock).mockResolvedValueOnce({
+      data: mockResponseData,
+    });
+
+    await act(async () => {
+      render(<Latest />);
+    });
 
     await waitFor(() => {
       expect(screen.queryByText("Loading...")).toBeNull();
@@ -34,14 +34,36 @@ describe("Latest", () => {
     expect(screen.getByText("Latest Blog")).toBeInTheDocument();
   });
 
-  it("renders loading indicator initially", () => {
-    render(<Latest />);
-    expect(screen.getByText("Loading...")).toBeInTheDocument();
+  it("renders loading indicator initially", async () => {
+    (axios.get as jest.Mock).mockImplementationOnce(() =>
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve({ data: mockResponseData });
+        }, 1000);
+      })
+    );
+
+    await act(async () => {
+      render(<Latest />);
+    });
+
+    jest.advanceTimersByTime(1000);
+
+    await waitFor(() => {
+      expect(screen.queryByText("Loading...")).toBeNull();
+    });
+
+    expect(screen.getByText("Latest Project")).toBeInTheDocument();
+    expect(screen.getByText("Latest Blog")).toBeInTheDocument();
   });
 
   it("renders error message when API call fails", async () => {
     (axios.get as jest.Mock).mockRejectedValueOnce(new Error("API Error"));
-    render(<Latest />);
+
+    await act(async () => {
+      render(<Latest />);
+    });
+
     await waitFor(() => {
       expect(
         screen.getByText("An error occurred while fetching data.")
@@ -52,11 +74,14 @@ describe("Latest", () => {
   });
 
   it("renders latest portfolio and blog titles when API call succeeds", async () => {
-    // manually mock axios as the error test above is happening asynchronously and changing the mock values
     (axios.get as jest.Mock).mockResolvedValueOnce({
       data: mockResponseData,
     });
-    render(<Latest />);
+
+    await act(async () => {
+      render(<Latest />);
+    });
+
     await waitFor(() => {
       expect(screen.getByText(mockPortfolio.title)).toBeInTheDocument();
       expect(screen.getByText(mockBlog.title)).toBeInTheDocument();
