674869290b77378b82afff161bc3d120dc6d6061
Aaron Lambley <aaron.lambley@gmail.com>
2024-10-14 15:05:57 +0100

    use sharp for prod images

--- __tests__/e2e/selectors/homePage.ts
+++
@@ -1,7 +0,0 @@
-export const HomePageSelectors = {
-  headline: "h3",
-  typewriter: ".text-center.fs-1.fw-bold.mb-5",
-  avatar: "img.round-avatar",
-  downloadLink: "a.download-cta",
-  bioTexts: "p.text-center",
-};
